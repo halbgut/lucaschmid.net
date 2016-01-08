96da1257f46be63312bdcaf7f8b31850fa4227cf
Kriegslustig <github@ls7.ch>
2016-01-08 17:15:47 +0100

    Add server-side only options

--- common/config.js
+++ common/config.js
@@ -1,8 +1,14 @@
 const _ = require('lodash')
-const conf = require(`../config.json`)
+const env = require('./lib/env')
+
+var conf = require(`../config.json`)
 
 if (process.NODE_ENV !== 'production') {
   conf = _.extend(conf, conf.dev)
+}
+
+if(env === 'server') {
+  conf = _.extend(conf, require('../server/config.json'))
 }
 
 module.exports = _.extend(conf, {
