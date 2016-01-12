9082745600cccceb27a4c86e1d9b3963d731efdb
Kriegslustig <github@ls7.ch>
2016-01-12 21:01:48 +0100

    Add a simple cache for common routes

--- server/lib/routes.js
+++ server/lib/routes.js
@@ -8,19 +8,26 @@ const config = require('../../common/config')
 const serverRoutes = require('../routes')
 const commonRoutes = require('../../common/routes')
 const fail = require('./fail')
+const cache = {}
 
 module.exports = () => {
   _.each(commonRoutes, (genParams, route) => {
     router.get(route, function *(next) {
       const that = this
       const params = genParams(this.params)
+      const cacheKey = params[2] || params[1]
+      // Use cached version if it's around
+      if (cache[cacheKey]) {
+        that.body = cache[cacheKey]
+        return
+      }
       yield parallelPromise([
         view.xhr(params[0], true),
         params[1]()
       ])
         .then(results => {
           const content = handlebars.compile(results[0][1])(results[1])
-          that.body = handlebars.compile(results[0][0], config)({ content })
+          that.body = cache[cacheKey] = handlebars.compile(results[0][0], config)({ content })
         })
         .catch(err => fail(err))
     })
