75cceff3c93b3ca9648517ada80b283ebc4543f5
Kriegslustig <github@ls7.ch>
2016-02-20 14:39:34 +0100

    Fix a security header

--- server/lib/security.js
+++ server/lib/security.js
@@ -1,8 +1,8 @@
 const csp =
-  "default-src 'self' raw.githubusercontent.com https:;"
+  "default-src 'self' https:;"
   + "style-src 'self' https: 'unsafe-inline';"
   + "script-src 'self' https: 'unsafe-eval';"
-  + "connect-src 'self' ws: wss:`"
+  + "connect-src 'self' raw.githubusercontent.com https: ws: wss:`"
 
 module.exports = function *(next) {
   this.response.set('Strict-Transport-Security', 'strict-transport-security: max-age=31536000; includeSubdomains')
