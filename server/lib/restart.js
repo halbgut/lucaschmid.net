9ee0e6695211d4ca8d97241e1965f089b0b46eb4
Kriegslustig <github@ls7.ch>
2016-03-18 09:13:24 +0100

    Refactor server/main a bit

---
+++ server/lib/restart.js
@@ -0,0 +1,30 @@
+// Github `push` webhook
+// This simple writes a file when the hook is called. The systemd-unit defined
+// in lucaschmid.net.restart.service pulls the latest version from the github
+// repo and restarts the docker container.
+
+const crypto = require('crypto')
+const scmp = require('scmp')
+const fs = require('fs')
+
+const config = require('../common/config.js')
+
+module.exports = function *(next) {
+  if (
+    this.method === 'POST' &&
+    this.url === '/restart'
+  ) {
+    const hmac = crypto.createHmac('sha1', config.restartKey)
+    const hash = hmac.update(JSON.stringify(this.request.body))
+    if (scmp('sha1=' + hash.digest('hex'), this.request.headers['x-hub-signature'])) {
+      fs.writeFileSync('restart', '.')
+      this.status = 200
+      this.body = ''
+    } else {
+      yield next
+    }
+  } else {
+    yield next
+  }
+}
+
--- server/main.js
+++ server/main.js
@@ -3,9 +3,6 @@
 const WebSocket = require('websocket')
 const WebSocketServer = WebSocket.server
 const http = require('http')
-const crypto = require('crypto')
-const fs = require('fs')
-const scmp = require('scmp')
 
 const koa = require('koa')
 const koaStatic = require('koa-static')
@@ -20,16 +17,14 @@ const websocketHandler = require('./lib/websocketHandler')
 const initTLS = require('./lib/tls')
 const security = require('./lib/security')
 const routes = require('./lib/routes')()
+const restart = require('./lib/restart')
 const config = require('../common/config.js')
 
 // Initialize app
 const app = koa()
 
-// Set the ports TODO: move this to the config somehow
-const ports = config.ports
-
 // Save the http server inside a const in order to use it later for the wss
-const server = http.createServer(app.callback()).listen(ports[0])
+const server = http.createServer(app.callback()).listen(config.ports[0])
 
 // Logging
 app.use(morgan.middleware('combined'))
@@ -48,27 +43,7 @@ app.use(security)
 app.use(bodyParser())
 
 // Github `push` webhook
-// This simple writes a file when the hook is called. The systemd-unit defined
-// in lucaschmid.net.restart.service pulls the latest version from the github
-// repo and restarts the docker container.
-app.use(function *(next) {
-  if (
-    this.method === 'POST' &&
-    this.url === '/restart'
-  ) {
-    const hmac = crypto.createHmac('sha1', config.restartKey)
-    const hash = hmac.update(JSON.stringify(this.request.body))
-    if (scmp('sha1=' + hash.digest('hex'), this.request.headers['x-hub-signature'])) {
-      fs.writeFileSync('restart', '.')
-      this.status = 200
-      this.body = ''
-    } else {
-      yield next
-    }
-  } else {
-    yield next
-  }
-})
+app.use(restart())
 
 // Static files
 app.use(koaStatic(`${__dirname}/../client`))
@@ -100,7 +75,7 @@ new WebSocketServer({ httpServer: server })
   .on('connect', websocketHandler.onSocketConn)
 
 // Mount the TLS-Server and the wss://
-initTLS('./tls/key.pem', './tls/cert.pem', app.callback(), ports[1])
+initTLS('./tls/key.pem', './tls/cert.pem', app.callback(), config.ports[1])
   .then((tlsServer) => {
     tls = true
     new WebSocketServer({ httpServer: tlsServer })
