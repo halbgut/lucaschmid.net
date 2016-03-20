5a80c4148202effd50b01d4f0b9ad3e203f6e125
Kriegslustig <github@ls7.ch>
2016-03-20 20:42:53 +0100

    Implement a image attribute for application sections

--- common/data/application/en/00_toc.md
+++ common/data/application/en/00_toc.md
@@ -1,5 +1,7 @@
 # Table Of Contents
 
+[image:/img/application/tumblr_o1dennYMHW1sfie3io1_1280.jpg]
+
 * [Me](#me)
 * [Early Career](#early-career)
 * [My Work In Open-Source](#open-source)
--- server/lib/getMarkdown.js
+++ server/lib/getMarkdown.js
@@ -1,17 +1,35 @@
+'use strict'
 const showdown = require('showdown')
 const footnotes = require('showdown-footnotes')
 const fs = require('fs')
 const path = require('path')
 const glob = require('glob')
 const async = require('async')
+const _ = require('lodash')
 
 const config = require('../../common/config.js')
 
-const ctimeExp = /\[ctime:(\d+)\]/
 const paragraphsExp = /<p>(.*)<\/p>/
 const titleExp = /<h1( id=\"\w*\")?>(.*)<\/h1>/ // The regex narrow implementation that only works well for showdown
 
 const mdConverter = new showdown.Converter({ extensions: [footnotes] })
+
+const blocks = [
+  [
+    'ctime',
+    /\[ctime:(\d+)\]/,
+    (data) => {
+      let ctime = new Date()
+      ctime.setTime(data[1] || 0)
+      return ctime
+    }
+  ],
+  [
+    'image',
+    /\[image:([^\]]+)\]/,
+    (data) => data[1]
+  ]
+]
 
 module.exports = (pattern) => {
   return new Promise((resolve, reject) => {
@@ -24,17 +42,20 @@ module.exports = (pattern) => {
         (file, next) => {
           fs.readFile(file, { encoding: 'utf8' }, (err, data) => {
             if (err) next(err)
-            const ctime = new Date()
-            const html = mdConverter.makeHtml(data.replace(ctimeExp, ''))
-            ctime.setTime((ctimeExp.exec(data) || [])[1] || 0)
-            next(null, {
+            const foundBlocks = {}
+            blocks.forEach((block) => {
+              foundBlocks[block[0]] = block[2](block[1].exec(data) || [])
+              data = data.replace(block[1], '')
+            })
+            console.log(foundBlocks)
+            const html = mdConverter.makeHtml(data)
+            next(null, _.assign({
               html,
               paragraphs: paragraphsExp.exec(html),
               title: titleExp.exec(html)[2],
               name: path.basename(file).split('.').slice(0, -1).join('.'),
-              ctime,
               file
-            })
+            }, foundBlocks))
           })
         },
         (err, data) => {
