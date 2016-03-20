4eadc4f40f6ae119ab6e09d8354e48aededa844a
Kriegslustig <github@ls7.ch>
2016-03-20 15:55:56 +0100

    Implement queing for the client-side api

--- client/js/lib/api.js
+++ client/js/lib/api.js
@@ -10,32 +10,49 @@ const wsApiReq = (url, socket) => new Promise((resolve, reject) => {
   socket.send(url)
 })
 
-module.exports = () =>
-  (
-    (queue, ws, wsSupport) =>
-      (api, method) => new Promise((resolve, reject) => {
-        const doXhrReq = () =>
-          xhr(`/api/${api}/xhr/${method}`)
-            .then((data) => resolve(JSON.parse(data)))
-            .catch(reject)
-        const doWsReq = () =>
-          wsApiReq(`/api/${api}/ws/${method}`, ws)
-            .then(resolve)
-            .catch(reject)
+const queue = []
+let lock = false
+let ws = undefined
+let wsSupport = true
 
-        if (!ws) {
-          websocket()
-            .then((newSocket) => {
-              ws = newSocket
-              doWsReq()
-            })
-            .catch((e) => {
-              wsSupport = false
-              doXhrReq()
-            })
-        } else {
-          doWsReq()
-        }
+const unlockAndCheck = (cb) =>
+  (data) => {
+    cb(data)
+    lock = false
+    checkQueue()
+  }
+
+const checkQueue = () => {
+  if (lock || queue.length === 0) return
+  const call = queue.shift()
+  lock = true
+  if (wsSupport && !ws) {
+    websocket()
+      .then((newSocket) => {
+        ws = newSocket
+        call[0]()
       })
-  )(false, undefined, true)
+      .catch((e) => {
+        wsSupport = false
+        call[1]()
+      })
+  } else if (ws) call[0]()
+  else call[1]()
+}
+
+module.exports = (api, method) => new Promise((resolve, reject) => {
+  const doXhrReq = () =>
+    xhr(`/api/${api}/xhr/${method}`)
+      .then(unlockAndCheck((data) =>
+        resolve(JSON.parse(data))
+      ))
+      .catch(unlockAndCheck(reject))
+  const doWsReq = () =>
+    wsApiReq(`/api/${api}/ws/${method}`, ws)
+      .then(unlockAndCheck(resolve))
+      .catch(unlockAndCheck(reject))
+  queue.push([doWsReq, doXhrReq])
+  checkQueue()
+})
+
 
