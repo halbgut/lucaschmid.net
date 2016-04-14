45514dcfe616c824a98d36b91bd1b7e679544164
Kriegslustig <kriegslustig@kriegslustig.me>
2016-04-14 19:31:55 +0200

    Add some comments about the RESUME.md

--- client/resume.js
+++ client/resume.js
@@ -6,3 +6,8 @@ require('./css/resume.css')
 
 riot.mount('*')
 
+console.log('%cHi!', 'text-decoration: blink; color: black; font-size: 30px; font-family: monospace;')
+console.log('You\'re probably here because you want to see some source code.')
+console.log('Check it out here: https://github.com/Kriegslustig/lucaschmid.net/blob/master/RESUME.md')
+console.log('I hope you\'ll like what you see. :)')
+
