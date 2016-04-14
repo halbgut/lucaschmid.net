137c07f396c9d301507fd4567de86a37de4f471a
Kriegslustig <kriegslustig@kriegslustig.me>
2016-04-14 19:02:21 +0200

    Add some comments for documentation to the resume route

--- server/lib/resumeRoute.js
+++ server/lib/resumeRoute.js
@@ -3,12 +3,28 @@ const view = require('./view')
 const fail = require('./fail')
 const getMarkdown = require('./getMarkdown')
 
+/**
+  * This function is called through a caching abstraction
+  * so it will only be executed once. Whenever I push to
+  * the Master branch on the Github remote, the server
+  * restarts and the cache is cleared. I can also manually
+  * invalidate the cache using a special route and a secret
+  * key.
+  */
 module.exports = (context) => new Promise((resolve, reject) => {
   Promise.all([
+    /* Parse all markdown files in /common/data/resume */
     getMarkdown('resume/*/*'),
+    /* Parse the resume Pug template */
     view.private.getTemplate('resume', false)
   ])
     .then((res) => {
+      /**
+       * All Markdown files inside /common/data/resume were
+       * inside directories named after language identifiers.
+       * The next block into a structure that is parsable by
+       * the template.
+       */
       const languages = []
       const chapters = res[0]
         .sort((c1, c2) => c1.name > c2.name ? 1 : -1)
@@ -22,7 +38,10 @@ module.exports = (context) => new Promise((resolve, reject) => {
           m[c.name].titles[lang] = c.title
           return m
         }, {})
+
+      /* Set an OK status code */
       context.status = 200
+      /* Compile the pug template and execute it */
       context.body = pug(res[1])({
         description: 'My Application for a position as a front-end engineer at Wimdu',
         lang: 'en',
@@ -30,8 +49,13 @@ module.exports = (context) => new Promise((resolve, reject) => {
         chapters,
         languages
       })
+      /* That's it. */
       resolve()
     })
+    /**
+      * If anything goes wrong, the `fail` function send
+      * an appropriate status code and message.
+      */
     .catch((err) => resolve(fail(context, err)))
 })
 
