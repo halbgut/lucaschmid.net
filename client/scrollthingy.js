a0f36c15208989823aabc739fab054af40ef9acb
Kriegslustig <kriegslustig@kriegslustig.me>
2016-04-17 20:14:07 +0200

    Implement a simple loading animation on the resume

--- client/scrollthingy.js
+++ client/scrollthingy.js
@@ -7,6 +7,11 @@ window.requestAnimationFrame(() => {
   link.rel = 'stylesheet'
   link.href = '/build/scrollthingy.css'
   const head = document.getElementsByTagName('head')[0]
+  link.addEventListener('load', () => {
+    const loader = document.querySelector('.loader')
+    setTimeout(() => loader.classList.remove('loader--active'), 1000) // Wait until it really has been rendered
+    setTimeout(() => { loader.style.display = 'none' }, 1800)
+  })
   head.parentNode.insertBefore(link, head)
 })
 
